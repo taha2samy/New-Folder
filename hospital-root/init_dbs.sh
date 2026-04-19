e5015a6d7d7e0e8cf8bdd2d23f1a632553112982
taha2samy <samyt8435@gmail.com>
2026-04-20 01:42:06 +0200

    chore: remove init_dbs.sh initialization script

--- hospital-root/init_dbs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE patient_db;
-    CREATE DATABASE clinical_db;
-    CREATE DATABASE billing_db;
-EOSQL
