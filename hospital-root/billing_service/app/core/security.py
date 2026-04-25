effa0f7224cf44dcfbdda6b880752bd109455442
taha2samy <samyt8435@gmail.com>
2026-04-25 23:12:13 +0300

    refactor: remove redundant decode_token function from security module

--- hospital-root/billing_service/app/core/security.py
+++ hospital-root/billing_service/app/core/security.py
@@ -9,9 +9,3 @@ def decode_token(token: str):
     except JWTError:
         return None
 
-def decode_token(token: str):
-    try:
-        payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.JWT_ALGORITHM])
-        return payload
-    except JWTError:
-        return None
