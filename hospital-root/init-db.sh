a7aee95ec46e95427756096f225f2ec9ffbb506d
taha2samy <samyt8435@gmail.com>
2026-04-26 09:22:36 +0300

    refactor: simplify database initialization by replacing dynamic loop with static SQL block

--- hospital-root/init-db.sh
+++ hospital-root/init-db.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
 set -e
 
-function create_database() {
-	local database=$1
-	local exists=$(psql -U "$POSTGRES_USER" -tAc "SELECT 1 FROM pg_database WHERE datname='$database'")
-	if [ "$exists" != "1" ]; then
-		psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-			CREATE DATABASE $database;
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	CREATE DATABASE patient_db;
+	CREATE DATABASE clinical_db;
+	CREATE DATABASE pharmacy_db;
+	CREATE DATABASE aggregator_db;
+	CREATE DATABASE laboratory_db;
+	CREATE DATABASE reference_db;
+	CREATE DATABASE keycloak_db;
+	CREATE DATABASE billing_db;
 EOSQL
-	fi
-}
-
-databases=(
-	"patient_db"
-	"clinical_db"
-	"pharmacy_db"
-	"aggregator_db"
-	"laboratory_db"
-	"reference_db"
-	"keycloak_db"
-	"billing_db"
-)
-
-for db in "${databases[@]}"; do
-	create_database $db
-done
